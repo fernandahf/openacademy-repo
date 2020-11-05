2d6fd76064989bad223a7e53dcb9803fde337bdf
Fernanda Hernandez <fernanda@vauxoo.com>
2020-11-05 10:30:40 -0600

    [REF] openacademy: Add duplicate test to openacademy course

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -3,7 +3,7 @@
 Global test to openacademy course model.
 Test create course and trigger constraints.
 '''
-from psycopg2.errors import CheckViolation
+from psycopg2.errors import CheckViolation, UniqueViolation
 
 from odoo.tests.common import TransactionCase
 from odoo.tools import mute_logger
@@ -31,7 +31,7 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
 
     # Mute error odoo.sql_db to avoid it in log
     @mute_logger('odoo.sql_db')
-    def test_01_same_name_and_description(self):
+    def test_10_same_name_and_description(self):
         '''
         Create a course with same name and description.
         To test constraint of name different to description.
@@ -45,4 +45,29 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
                 ):
             # Create a course with same name and description to raise error    
             self.create_course('Test', 'Test', None)
+
+    # Mute error odoo.sql_db to avoid it in log
+    @mute_logger('odoo.sql_db')
+    def test_20_two_courses_same_name(self):
+        '''
+        Test to create two courses with same name.
+        To raise constraint of unique name
+        '''
+        new_id = self.create_course('test', 'test_description', None)
+        print('new_id %s' % new_id)
+        with self.assertRaisesRegexp(
+            UniqueViolation,
+            'duplicate key value violates unique constraint '
+            '"openacademy_course_name_unique"'
+            ):
+            new_id2 = self.create_course('test', 'test_description2', None)
+            print('new_id2 %s' % new_id2)
+    
+    def test_30_duplicate_course(self):
+        '''
+        Test to duplicate a course and check that work fine.
+        '''
+        course = self.env.ref('openacademy.course0')
+        course_id = course.copy()
+        print('course_id %s' % course_id)
 
